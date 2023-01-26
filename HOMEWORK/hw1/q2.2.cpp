65371a00be01b8c6fa97c80b4bef0360a5bbf124
Kasitphoom Thowongs <kasitphoom47@gmail.com>
2023-01-27 00:16:19 +0700

    C++ Term 2

---
+++ HOMEWORK/hw1/q2.2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+double FarenhitToCelsius(double farenhit){
+    return (farenhit - 32) * 5 / 9;
+}
+
+int main(){
+    int fahr = 0;
+    std::cout << std::fixed << std::setprecision(1);
+
+    cout << "Fahr\tCelcius" << endl;
+    while(fahr <= 300){
+        cout << fahr << "\t" << FarenhitToCelsius(fahr) << endl;
+        fahr += 40;
+    }
+}
