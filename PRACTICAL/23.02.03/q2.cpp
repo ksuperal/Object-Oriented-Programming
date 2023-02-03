3126c17c6b849016381069a8b82262fbade89dd1
Kasitphoom Thowongs <kasitphoom47@gmail.com>
2023-02-03 13:53:50 +0700

    C++ Term 2

--- PRACTICAL/23.02.03/q2.cpp
+++ PRACTICAL/23.02.03/q2.cpp
@@ -45,15 +45,44 @@ void print_words_commas(const vector<string>& words, std::ostream& out){
     }
 }
 
+auto extract_attrs2(const vector<string>& words, vector<string>& attrs, vector<string>& non_attrs){
+    for (const auto& w: words){
+        if (w.find("[[") != string::npos && w.find("]]") != string::npos){
+            attrs.push_back(w);
+        } else {
+            non_attrs.push_back(w);
+        }
+    }
+}
+
+std::ostream& print_words_new(std::ostream& out, const vector<string>& words){
+    for (const auto& w: words){
+        out << w << " ";
+    }
+    return out;
+}
+
+std::ostream& print_words_commas_new(std::ostream& out, const vector<string>& words){
+    int n = words.size();
+    for (int i = 0; i < n; i++){
+        out << words[i];
+        if (i < n - 1){
+            out << ", ";
+        }
+    }
+    return out;
+}
+
 int main()
 {
+    cout << endl;
+    cout << "=====================2.3=====================" << endl;
     const vector<string> test_words{
     "switch", "[[noreturn]]",
     "if", "[[maybe_unused",
     "for", "fallthrough"
     };
 
-    cout << endl;
     auto non_attrs = test_words;
     auto attrs = extract_attrs(non_attrs);
     cout << "Test words: ";
@@ -77,5 +106,31 @@ int main()
     cout << "Non attributes: ";
     print_words_commas(non_attrs, cout);
     cout << endl;
+
+    cout << endl;
+    cout << "=====================2.4=====================" << endl;
+
+    vector<string> attrs_new, non_attrs_new;
+    extract_attrs2(
+    test_words, attrs_new, non_attrs_new);
+    cout << "Test words: ";
+    print_words_new(cout, test_words) << endl;
+    cout << "Attributes: ";
+    print_words_new(cout, attrs_new) << endl;
+    cout << "Non attributes: ";
+    print_words_new(cout, non_attrs_new) << endl;
+    cout << "Attributes: ";
+    print_words_new(cout, attrs_new) << endl;
+
+    cout << endl;
+    cout << "Test words: ";
+    print_words_commas_new(cout, test_words) << endl;
+    cout << "Attributes: ";
+    print_words_commas_new(cout, attrs_new) << endl;
+    cout << "Non attributes: ";
+    print_words_commas_new(cout, non_attrs_new) << endl;
+    cout << "Attributes: ";
+    print_words_commas_new(cout, attrs_new) << endl;
+    
     return 0;
 }
