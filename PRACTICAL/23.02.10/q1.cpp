f7f143ca01230ad2affbaa22a8af102f9946f390
Kasitphoom Thowongs <kasitphoom47@gmail.com>
2023-02-10 13:37:22 +0700

    C++ Term 2

--- PRACTICAL/23.02.10/q1.cpp
+++ PRACTICAL/23.02.10/q1.cpp
@@ -11,7 +11,7 @@ using std::vector;
 bool is_prime(int n)
 {   
     if (n == 2 || n == 3) return true; // fixed this
-    if ((n % 2 == 0 || n % 3 == 0)) return false;
+    if ((n % 2 == 0 || n % 3 == 0 || n % 5 == 0)) return false;
     // check through all numbers of the form i = 6k - 1 and i = 6k + 1
     for (int i = 5; i * i < n; i += 6) {
         if (n % i == 0 || n % (i + 2) == 0) return false; // n is divisible by i or i + 2
@@ -25,6 +25,7 @@ T median(std::vector<T> vec)
     using vec_sz = typename vector<T>::size_type;
     vec_sz size = vec.size();
     if (size == 0) throw std::domain_error("median of an empty vector");
+    if (size == 1) return vec[0];
 
     sort(vec.begin(), vec.end());
 
@@ -33,18 +34,21 @@ T median(std::vector<T> vec)
 }
 
 int main()
-{
+{   
+    cout << "======================1.1======================" << endl;
     cout << "Enter the upper limit: ";
     int n;
     cin >> n;
-    for (int i = 2; i <= n; i += 1) { // fixed this
+    for (int i = 2; i <= n; i++) { // fixed this
         // cout << i << endl;
         if (is_prime(i)) cout << i << endl;
     }
 
-    // std::vector<int> vec_sample {1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 11, 12, 13, 14, 15, 16};
+    cout << endl;
+    cout << "======================1.2======================" << endl;
+    std::vector<int> vec_sample {1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 11, 12, 13, 14, 15, 16};
 
-    // cout << median<int>(vec_sample) << endl;
+    cout << median<int>(vec_sample) << endl;
 
     return 0;
 }
--- PRACTICAL/23.02.10/q3.cpp
+++ PRACTICAL/23.02.10/q3.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <list>
 #include <string>
+#include <map>
 
 using std::cout;
 using std::cin;
@@ -13,6 +14,7 @@ using std::list;
 using std::copy;
 using std::string;
 using std::istream;
+using std::map;
 
 istream& read_input(istream& in, vector<string>& vec){
 
@@ -27,29 +29,73 @@ istream& read_input(istream& in, vector<string>& vec){
     return in;
 }
 
+map<string, double> name_amount(vector<string> name, vector<double> amount){
+    map<string, double> name_amount;
+    for (int i = 0; i < name.size(); i++){
+        if (name_amount.find(name[i]) != name_amount.end()){
+            name_amount[name[i]] += amount[i];
+        } else {
+            name_amount[name[i]] = amount[i];
+        }
+    }
+    return name_amount;
+}
+
+map<string, string> name_amountcomma(vector<string> name, vector<int> amount){
+    map<string, string> name_amountcomma;
+    for (int i = 0; i < name.size(); i++){
+        if (name_amountcomma.find(name[i]) != name_amountcomma.end()){
+            name_amountcomma[name[i]] += ", " + std::to_string(amount[i]);
+        } else {
+            name_amountcomma[name[i]] = std::to_string(amount[i]);
+        }
+    }
+    return name_amountcomma;
+}
+
 int main(){
 
-    cout << "======================3.1======================" << endl;
+    // cout << "======================3.1======================" << endl;
+    // cout << "Enter the list of word and number: ";
+    // vector<string> input;
+    // read_input(cin, input);
+
+    // vector<string> name;
+    // vector<double> amount;
+    // for (const auto& h: input){
+    //     try {
+    //         double x = std::stod(h);
+    //         amount.push_back(x);
+    //     } catch (std::invalid_argument& e){
+    //         name.push_back(h);
+    //     }
+    // }
+
+    // map<string, double> out = name_amount(name, amount);
+    // for (const auto& h: out){
+    //     cout << h.first << " " << h.second << endl;
+    // }
+
+    // cout << endl;
+    cout << "======================3.2======================" << endl;
     cout << "Enter the list of word and number: ";
     vector<string> input;
     read_input(cin, input);
 
-    for (const auto& h: input){
-        cout << h << " ";
-    }
-
-    vector<string> word;
-    vector<double> number;
+    vector<string> name;
+    vector<int> amount;
     for (const auto& h: input){
         try {
-            double x = std::stod(h);
-            number.push_back(x);
+            int x = std::stoi(h);
+            amount.push_back(x);
         } catch (std::invalid_argument& e){
-            word.push_back(h);
+            name.push_back(h);
         }
     }
-
-    cout << endl << "The list of word: " << endl;
+    map<string, string> out = name_amountcomma(name, amount);
+    for (const auto& h: out){
+        cout << h.first << " " << h.second << endl;
+    }
 
     return 0;
 }
